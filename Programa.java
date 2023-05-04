1f32a83457e676afafb9fd730d616a8722cf8784
guribaby <122307456+guribaby@users.noreply.github.com>
2023-05-03 20:46:04 -0600

    Create Programa.java

---
+++ Programa.java
@@ -0,0 +1,15 @@
+package act15;
+
+
+public class Programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		matriz z = new matriz ();
+
+		System.out.println(z.sumaMatriz());	
+
+	}
+
+}
